bfef81cc66f6fb699ca6219687d820585b3ee750
Wellington <wellingtoncarvalho11@gmail.com>
2022-03-23 20:25:59 -0300

    feature: operacoes basicas com S3

--- Aws/src/main/java/com/amazon/s3/S3.java
+++ Aws/src/main/java/com/amazon/s3/S3.java
@@ -5,8 +5,9 @@ import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
-import com.amazonaws.services.s3.model.Bucket;
+import com.amazonaws.services.s3.model.*;
 
+import java.io.File;
 import java.util.List;
 
 public class S3 {
@@ -24,15 +25,35 @@ public class S3 {
                 .withRegion(Regions.SA_EAST_1)
                 .build();
 
-        List<Bucket> buckets = s3.listBuckets();
+        System.out.printf("Criando Buckets");
+        String bucketName = "wellingtonnovo";
+        s3.createBucket(bucketName);
+
 
         System.out.println("Lista de Buckets...");
+        List<Bucket> buckets = s3.listBuckets();
         buckets.forEach(item -> {
             System.out.println(item.getName());
         });
 
+        System.out.printf("UpLoad de Arquivo");
+        String diretorioArquivo = "";
+        File file = new File(diretorioArquivo);
+        s3.putObject(bucketName, "name-destino", file);
+
+        System.out.printf("Listando Objetos");
+        ListObjectsRequest listObjectsRequest = new ListObjectsRequest().withBucketName(bucketName);
+        ObjectListing listObjects = s3.listObjects(listObjectsRequest);
+        for (S3ObjectSummary objetSummary : listObjects.getObjectSummaries()) {
+            System.out.println("*" + objetSummary.getKey() + " - "+ objetSummary.getSize());
+        }
+
+        System.out.printf("Apagando Arquivos do Bucket");
+        s3.deleteObject(bucketName,"keyOfObject");
 
 
+        System.out.printf("Apagando Bucket Of S3");
+        s3.deleteBucket(bucketName);
 
     }
 }
